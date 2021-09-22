31aca9a63260321599a2509d431c2e625dee21b8
taoth <ngvmanh129@gmail.com>
2021-09-22 16:05:48 +0300

    update task 1_4

---
+++ src/ru/mirea/task1/opt4/Task_4.java
@@ -0,0 +1,12 @@
+package ru.mirea.task1.opt4;
+
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
+
